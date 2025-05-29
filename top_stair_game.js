4a7d9652c93f651ba88927f78b9970cd85ead31e
wslider <wslider2000@gmail.com>
2025-05-29 13:14:37 -0400

    7 score bonus round. add easterEgg later

--- top_stair_game.js
+++ top_stair_game.js
@@ -109,10 +109,10 @@ function virtualTurn(round) {
         currentRound++;
         setTimeout(() => startRound(currentRound), 2000); // Wait before next round
     } else {
-        if (playerScores [playerScores.length - 1] >= 20) {
+        if (playerScores.includes(7)) {
             bonusPlayerTurn();
         }
-        else if (playerScores [playerScores.length - 2] >= 20) {
+        else if (playerScores [playerScores.length - 1] >= 20) {
             bonusPlayerTurn();
         }
        else {
